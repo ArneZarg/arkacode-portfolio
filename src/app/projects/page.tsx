7b99507799043c7ba7325580ae9bf33dae991f1c
ArneZarg <zargarian.arne@gmail.com>
2023-07-16 13:14:42 -0700

    style changes, masonry build

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,58 @@
-import styles from '../page.module.css'
+import styles from './projects.module.css'
+import Image from 'next/image';
 
-const Second = () => {
-    return(<main className={styles.main}><h1>hey everyone</h1></main>)
+const Projects = () => {
+    return(
+        <main>
+            <div className={styles.container}>
+                <h1>Projects</h1>
+                <div className={styles.projects}>
+                    <div className={styles.project}>
+                        <Image
+                            src={'/img/310nutritionportfolio.gif'}
+                            width={500}
+                            height={500}
+                            alt={"310gif"}
+                        />
+                        <div className={styles.description}>
+                            <div className={styles.text}></div>
+                        </div>
+                    </div>
+                    <div className={styles.project}>
+                        <Image
+                            src={'/img/hpemt.gif'}
+                            width={500}
+                            height={500}
+                            alt={"hpemt"}
+                        />
+                    </div>
+                    <div className={styles.project}>
+                        <Image
+                            src={'/img/kkportfolio.gif'}
+                            width={500}
+                            height={500}
+                            alt={"hpemt"}
+                        />
+                    </div>
+                    <div className={styles.project}>
+                        <Image
+                            src={'/img/kkportfolio.gif'}
+                            width={500}
+                            height={500}
+                            alt={"hpemt"}
+                        />
+                    </div>
+                    <div className={styles.project}>
+                        <Image
+                            src={'/img/sanMarino.gif'}
+                            width={500}
+                            height={500}
+                            alt={"sanMarino"}
+                        />
+                    </div>
+                </div>
+            </div>
+        </main>)
 }
 
-export default Second;
+export default Projects;
