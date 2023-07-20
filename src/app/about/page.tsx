d7ae0f074d02df052e9068047ccc744437230c52
ArneZarg <zargarian.arne@gmail.com>
2023-07-19 20:44:43 -0700

    footer of about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import Image from "next/image";
+import styles from "./about.module.css"
+import Skill from "@/components/about/skill";
+import { useState,useEffect } from "react";
+
+
+const getData = async () => {
+    try {
+      const response = await fetch('/api/about');
+      const data = await response.json();
+      console.log('Data: ', data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+      throw error;
+    }
+  };
+
+export default function About(){
+
+    const [skills, setSkills] = useState<any>([]);
+
+    useEffect(() => {
+        getData()
+        .then((data) => setSkills(data))
+        .catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
+    return(
+        <main>
+            <div className={styles.container}>
+                <h1>Why Choose Arne Zargarian?</h1>
+                <div className={styles.skills}>
+                    {
+                        skills.map((s:any) => {
+                            return <Skill skillData={s}/>
+                        })
+                    }
+                </div>
+                <a href="Arne Zargarian - Full Stack Developer.docx.pdf" target="_blank">Download my resume here!</a>
+                <div className={styles.footer}>
+                    <h2>Ready to bring your digital vision to life?</h2>
+                    <p>Reach out to Arne today to discuss your web development needs. With his proven expertise and commitment to excellence, John will work closely with you to turn your ideas into reality. Let's create something remarkable together!</p>
+                </div>
+            </div>
+        </main>
+    )
+}
